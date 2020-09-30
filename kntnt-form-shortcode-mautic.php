1902bd97bd4802b41aa4452abb9e52862a7703de
Thomas Barregren <thomas@kntnt.se>
2020-09-30 19:01:04 +0200

    Disabled debugging. Ready to use.

--- kntnt-form-shortcode-mautic.php
+++ kntnt-form-shortcode-mautic.php
@@ -5,7 +5,7 @@
  * Plugin Name:       Kntnt Form Shortcode for Mautic
  * Plugin URI:        https://github.com/kntnt/kntnt-form-shortcode-mautic
  * GitHub Plugin URI: https://github.com/kntnt/kntnt-form-shortcode-mautic
- * Description:       Allows Kntnt Form Shortcode (KFS) to post to Mautic.
+ * Description:       Allows posting to Mautic from Kntnt Form Shortcode (KFS).
  * Version:           1.0.0
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
@@ -19,7 +19,7 @@
 namespace Kntnt\Form_Shortcode_Mautic;
 
 // Uncomment following line to debug this plugin.
-define( 'KNTNT_FORM_SHORTCODE_MAUTIC', true );
+// define( 'KNTNT_FORM_SHORTCODE_MAUTIC', true );
 
 require 'vendor/autoload.php';
 
