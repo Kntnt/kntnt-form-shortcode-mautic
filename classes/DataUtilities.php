7a1ed3e9c05db5676acad1921ad70417d36d3d64
Thomas Barregren <thomas@kntnt.se>
2020-10-02 22:47:56 +0200

    Replaced Option with the generalized DataUtilities.

--- classes/DataUtilities.php
+++ classes/DataUtilities.php
@@ -4,7 +4,7 @@
 namespace Kntnt\Form_Shortcode_Mautic;
 
 
-trait Option {
+trait DataUtilities {
 
     // The call `option()` returns an option named as this plugin if it exists
     // and is an array. If it doesn't exists or is not an array, false is
@@ -88,6 +88,22 @@ trait Option {
     // callable.
     public static final function evaluate( $value, ...$args ) {
         return is_callable( $value ) ? call_user_func( $value, ...$args ) : $value;
+    }
+
+    // Returns the value of the custom field `$field`. If a `get_field()`-
+    // function is provided by a plugin, e.g. ACF, it is called with `$field`
+    // as the only argument. If a `get_field()`-function doesn't exist,
+    // WordPress `get_metadata()`-function is called with all provided
+    // arguments.
+    public static final function get_field( $field, $post_id, $single = false, $type = 'post' ) {
+        if ( function_exists( 'get_field' ) ) {
+            // If ACF is installed, let it get the field.
+            return get_field( $field, $post_id );
+        }
+        else {
+            // If ACF not installed, let's do it ourselves.
+            return get_metadata( $type, $post_id, $field, $single );
+        }
     }
 
 }
